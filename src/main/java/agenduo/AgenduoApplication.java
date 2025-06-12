828f0d03268e3c904e0028e4a3906d2144732b14
Nicole Notebook <nicolesypriany@gmail.com>
2025-06-12 20:50:48 -0300

    initial commit

---
+++ src/main/java/agenduo/AgenduoApplication.java
@@ -0,0 +1,13 @@
+package agenduo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenduoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenduoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/agenduo/AgenduoApplicationTests.java
@@ -0,0 +1,13 @@
+package agenduo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenduoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
